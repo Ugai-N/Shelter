82ceaa0063d3dff1321d625d17c2d69f318045df
Nadezhda Ugay <777ugay@gmail.com>
2023-09-09 15:49:38 +0300

    FBV -> CBV

--- shelter/urls.py
+++ shelter/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from shelter.apps import ShelterConfig
-from shelter.views import index, contact, breeds, category_dogs
+from shelter.views import contact, BreedListView, DogListView, DogCreateView, DogUpdateView, DogDeleteView, IndexView
 
 app_name = ShelterConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('contact/', contact, name='contact'),
-    path('breeds/', breeds, name='breeds'),
-    path('<int:pk>/dogs/', category_dogs, name='category_dogs'),
+    path('breeds/', BreedListView.as_view(), name='breeds'),
+    path('dogs/<int:pk>', DogListView.as_view(), name='category_dogs'),
+    path('dogs/create/', DogCreateView.as_view(), name='create_dog'),
+    path('dogs/update/<int:pk>', DogUpdateView.as_view(), name='update_dog'),
+    path('dogs/delete/<int:pk>', DogDeleteView.as_view(), name='delete_dog'),
 ]
